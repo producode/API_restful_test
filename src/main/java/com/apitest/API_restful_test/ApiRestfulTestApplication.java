3df7e75f5fb0efcc79ed655b63c5a617932d8fdb
laugonzalezutn <laugonzalez@frba.utn.edu.ar>
2023-07-20 22:22:12 -0300

    first commit

---
+++ src/main/java/com/apitest/API_restful_test/ApiRestfulTestApplication.java
@@ -0,0 +1,13 @@
+package com.apitest.API_restful_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestfulTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestfulTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apitest/API_restful_test/ApiRestfulTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apitest.API_restful_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestfulTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
